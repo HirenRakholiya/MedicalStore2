73bd2a811c22d229d1b53782499e7a7d523c4bc5
JayLimbasiya2001 <84462763+JayLimbasiya2001@users.noreply.github.com>
2021-10-22 16:58:09 +0530

    commted by Jay

--- Controllers/TypeController.cs
+++ Controllers/TypeController.cs
@@ -3,43 +3,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using MedicalStore.Models;
 
 namespace MedicalStore.Controllers
 {
     public class TypeController : Controller
     {
+        string connectionString = @"Data Source=.;Initial Catalog=PharmaWholeSale;Integrated Security=True";
         // GET: Type
         public ActionResult Index()
         {
-            return View();
+            DataTable dt = new DataTable();
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                string query = "Select * from Type";
+                SqlDataAdapter adp = new SqlDataAdapter(query, con);
+                adp.Fill(dt);
+            }
+            return View(dt);
         }
 
         // GET: Type/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
+       
 
         // GET: Type/Create
         public ActionResult Create()
         {
-            return View();
+            return View(new TypeModel());
         }
 
         // POST: Type/Create
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        public ActionResult Create(TypeModel typeModel)
         {
-            try
+            using (SqlConnection con = new SqlConnection(connectionString))
             {
-                // TODO: Add insert logic here
+                con.Open();
+                string query = "Insert into Type Values(@TypeName)";
+                SqlCommand cmd = new SqlCommand(query, con);
+                // cmd.Parameters.AddWithValue("@Id", manufactureModel.Id);
+                cmd.Parameters.AddWithValue("@TypeName", typeModel.TypeName);
 
-                return RedirectToAction("Index");
+                cmd.ExecuteNonQuery();
             }
-            catch
-            {
-                return View();
-            }
+            return RedirectToAction("Index");
         }
 
         // GET: Type/Edit/5
---
+++ Models/TypeModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MedicalStore.Models
+{
+    public class TypeModel
+    {
+        public int Typeid { get; set; }
+
+        public string TypeName { get; set; }
+    }
+}
